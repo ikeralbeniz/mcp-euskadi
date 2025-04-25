5493d946948ce45091d39c36607bd29ed3611da1
Dani Latorre <danilatorre@genially.com>
2025-04-26 01:33:02 +0200

    Formatting the code

--- index.js
+++ index.js
@@ -4,138 +4,199 @@ import { z } from "zod";
 
 const server = new McpServer({
   name: "DNDzgz",
-  version: "0.0.1"
+  version: "0.0.1",
 });
 
-server.tool("zaragoza-tram-estimations", "Get the estimation of when arrives to a tram station in Zaragoza in realtime",
-  {station: z.number()},
-  async ({station}) => {
-    const response = await fetch(`https://dndzgz.herokuapp.com/services/tram/${station}`);
-    if(response.ok){
+server.tool(
+  "zaragoza-tram-estimations",
+  "Get the estimation of when arrives to a tram station in Zaragoza in realtime",
+  { station: z.number() },
+  async ({ station }) => {
+    const response = await fetch(
+      `https://dndzgz.herokuapp.com/services/tram/${station}`
+    );
+    if (response.ok) {
       const data = await response.json();
       return {
-        content: [{ type: "text", text: JSON.stringify(data) }]
+        content: [{ type: "text", text: JSON.stringify(data) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: `Is not possible to get the estimate for ${station}` }]
+        content: [
+          {
+            type: "text",
+            text: `Is not possible to get the estimate for ${station}`,
+          },
+        ],
       };
     }
   }
-)
+);
 
-server.tool("zaragoza-tram-stations", "Get all tram stations in Zaragoza",
+server.tool(
+  "zaragoza-tram-stations",
+  "Get all tram stations in Zaragoza",
   {
     latitude: z.number(),
-    longitude: z.number()
+    longitude: z.number(),
   },
-  async ({latitude, longitude}) => {
+  async ({ latitude, longitude }) => {
     const response = await fetch("https://dndzgz.herokuapp.com/services/tram");
-    if(response.ok){
+    if (response.ok) {
       const data = await response.json();
-      const nearestPositions = getTopNearestPositions(data.locations, latitude, longitude);
+      const nearestPositions = getTopNearestPositions(
+        data.locations,
+        latitude,
+        longitude
+      );
       return {
-        content: [{ type: "text", text: JSON.stringify(nearestPositions) }]
+        content: [{ type: "text", text: JSON.stringify(nearestPositions) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: "It was not possible to get the tram stations" }]
+        content: [
+          {
+            type: "text",
+            text: "It was not possible to get the tram stations",
+          },
+        ],
       };
     }
   }
-)
+);
 
-server.tool("zaragoza-bus-stops", "Get all bus stops in Zaragoza",
+server.tool(
+  "zaragoza-bus-stops",
+  "Get all bus stops in Zaragoza",
   {
     latitude: z.number(),
-    longitude: z.number()
+    longitude: z.number(),
   },
-  async ({latitude, longitude}) => {
+  async ({ latitude, longitude }) => {
     const response = await fetch("https://dndzgz.herokuapp.com/services/bus");
-    if(response.ok){
+    if (response.ok) {
       const data = await response.json();
-      const nearestPositions = getTopNearestPositions(data.locations, latitude, longitude);
+      const nearestPositions = getTopNearestPositions(
+        data.locations,
+        latitude,
+        longitude
+      );
       return {
-        content: [{ type: "text", text: JSON.stringify(nearestPositions) }]
+        content: [{ type: "text", text: JSON.stringify(nearestPositions) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: "It was not possible to get the bus stops" }]
+        content: [
+          { type: "text", text: "It was not possible to get the bus stops" },
+        ],
       };
     }
   }
-)
+);
 
-server.tool("zaragoza-bus-estimations", "Get the estimation of when a bus arrives to a stop in Zaragoza in realtime",
-  {stop: z.number()},
-  async ({stop}) => {
-    const response = await fetch(`https://dndzgz.herokuapp.com/services/bus/${stop}`);
-    if(response.ok){
+server.tool(
+  "zaragoza-bus-estimations",
+  "Get the estimation of when a bus arrives to a stop in Zaragoza in realtime",
+  { stop: z.number() },
+  async ({ stop }) => {
+    const response = await fetch(
+      `https://dndzgz.herokuapp.com/services/bus/${stop}`
+    );
+    if (response.ok) {
       const data = await response.json();
       return {
-        content: [{ type: "text", text: JSON.stringify(data) }]
+        content: [{ type: "text", text: JSON.stringify(data) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: `Is not possible to get the estimate for the stop ${stop}` }]
+        content: [
+          {
+            type: "text",
+            text: `Is not possible to get the estimate for the stop ${stop}`,
+          },
+        ],
       };
     }
   }
-)
+);
 
-server.tool("zaragoza-bizi-stations", "Get all Bizi stations in Zaragoza, the bicycle rental public service",
+server.tool(
+  "zaragoza-bizi-stations",
+  "Get all Bizi stations in Zaragoza, the bicycle rental public service",
   {
     latitude: z.number(),
-    longitude: z.number()
+    longitude: z.number(),
   },
-  async ({latitude, longitude}) => {
+  async ({ latitude, longitude }) => {
     const response = await fetch("https://dndzgz.herokuapp.com/services/bizi");
-    if(response.ok){
+    if (response.ok) {
       const data = await response.json();
-      const nearestPositions = getTopNearestPositions(data.locations, latitude, longitude);
+      const nearestPositions = getTopNearestPositions(
+        data.locations,
+        latitude,
+        longitude
+      );
       return {
-        content: [{ type: "text", text: JSON.stringify(nearestPositions) }]
+        content: [{ type: "text", text: JSON.stringify(nearestPositions) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: "It was not possible to get the Bizi stations" }]
+        content: [
+          {
+            type: "text",
+            text: "It was not possible to get the Bizi stations",
+          },
+        ],
       };
     }
   }
-)
+);
 
-server.tool("zaragoza-bizi-estimations", "Get the estimation of bikes and free slots in a Bizi station in Zaragoza in realtime",
-  {station: z.number()},
-  async ({station}) => {
-    const response = await fetch(`https://dndzgz.herokuapp.com/services/bizi/${station}`);
-    if(response.ok){
+server.tool(
+  "zaragoza-bizi-estimations",
+  "Get the estimation of bikes and free slots in a Bizi station in Zaragoza in realtime",
+  { station: z.number() },
+  async ({ station }) => {
+    const response = await fetch(
+      `https://dndzgz.herokuapp.com/services/bizi/${station}`
+    );
+    if (response.ok) {
       const data = await response.json();
       return {
-        content: [{ type: "text", text: JSON.stringify(data) }]
+        content: [{ type: "text", text: JSON.stringify(data) }],
       };
-    }else{
+    } else {
       return {
-        content: [{ type: "text", text: `Is not possible to get the estimate for the Bizi station ${station}` }]
+        content: [
+          {
+            type: "text",
+            text: `Is not possible to get the estimate for the Bizi station ${station}`,
+          },
+        ],
       };
     }
   }
-)
-server.tool("google-maps-link", "Get a Google Maps link from coordinates to help people to find a station o stop",
+);
+server.tool(
+  "google-maps-link",
+  "Get a Google Maps link from coordinates to help people to find a station o stop",
   {
     latitude: z.number(),
-    longitude: z.number()
+    longitude: z.number(),
   },
-  async ({latitude, longitude}) => {
+  async ({ latitude, longitude }) => {
     const mapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
     return {
-      content: [{ type: "text", text: mapsUrl }]
+      content: [{ type: "text", text: mapsUrl }],
     };
   }
-)
+);
 
-server.tool("geolocation-from-address", "Get the geolocation (latitude and longitude) from an address and the formatted address that was found, only for Zaragoza. Can be used to find a bus stops, tram stations or bizi stations",
+server.tool(
+  "geolocation-from-address",
+  "Get the geolocation (latitude and longitude) from an address and the formatted address that was found, only for Zaragoza. Can be used to find a bus stops, tram stations or bizi stations",
   {
-    address: z.string()
+    address: z.string(),
   },
   async ({ address }) => {
     const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
@@ -150,33 +211,51 @@ server.tool("geolocation-from-address", "Get the geolocation (latitude and longi
         location.confindence = data.results[0].geometry.location_type;
         location.formatted_address = data.results[0].formatted_address;
         return {
-          content: [{ type: "text", text: JSON.stringify(location) }]
+          content: [{ type: "text", text: JSON.stringify(location) }],
         };
       } else {
         return {
-          content: [{ type: "text", text: `No geolocation found for the address: ${address}` }]
+          content: [
+            {
+              type: "text",
+              text: `No geolocation found for the address: ${address}`,
+            },
+          ],
         };
       }
     } else {
       return {
-        content: [{ type: "text", text: `Failed to fetch geolocation for the address: ${address}` }]
+        content: [
+          {
+            type: "text",
+            text: `Failed to fetch geolocation for the address: ${address}`,
+          },
+        ],
       };
     }
   }
 );
 
-function getTopNearestPositions(positions, latitude, longitude, size=10){
-  return getOrderedPositionsByDistance(positions, latitude, longitude).slice(0, size);
+function getTopNearestPositions(positions, latitude, longitude, size = 10) {
+  return getOrderedPositionsByDistance(positions, latitude, longitude).slice(
+    0,
+    size
+  );
 }
 
 function getOrderedPositionsByDistance(positions, latitude, longitude) {
-  return positions.map((position) => {
-    const distanceInMeters = haversineDistanceInMeters({ lat: latitude, lon: longitude }, position);
-    return {
-      ...position,
-      distanceInMeters
-    };
-  }).sort((a, b) => a.distanceInMeters - b.distanceInMeters);
+  return positions
+    .map((position) => {
+      const distanceInMeters = haversineDistanceInMeters(
+        { lat: latitude, lon: longitude },
+        position
+      );
+      return {
+        ...position,
+        distanceInMeters,
+      };
+    })
+    .sort((a, b) => a.distanceInMeters - b.distanceInMeters);
 }
 
 function haversineDistanceInMeters(position1, position2) {
@@ -186,9 +265,12 @@ function haversineDistanceInMeters(position1, position2) {
   const dLat = toRadians(position2.lat - position1.lat);
   const dLon = toRadians(position2.lon - position1.lon);
 
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRadians(position1.lat)) * Math.cos(toRadians(position2.lat)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(position1.lat)) *
+      Math.cos(toRadians(position2.lat)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return radiusOfEarth * c * 1000;
